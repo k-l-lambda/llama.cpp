afcbf53292b5184314650e7b18a111d1eda80505
jifa <jifa@pplab.org>
2025-02-04 18:53:15 +0800

    convert_gguf_to_safetensors.py: refactored as cocurrent downloading.

--- convert_gguf_to_safetensors.py
+++ convert_gguf_to_safetensors.py
@@ -6,6 +6,7 @@ import torch
 import gguf
 from safetensors.torch import save_file
 from tqdm import tqdm
+import asyncio
 
 
 
@@ -33,17 +34,23 @@ def main (repo_path: str, local_dir: str, download_only: bool = False):
 	gguf_files = [file[len(repo_id) + 1:] for file in files if file.endswith('.gguf')]
 	#print('gguf_files:', gguf_files)
 
-	for file in gguf_files:
+	async def download_and_convert(file):
 		gguf_path = os.path.join(local_dir, file)
 		if not os.path.exists(gguf_path):
 			print(f'Downloading {repo_id}/{file}...')
-			gguf_path = hf_hub_download(repo_id=repo_id, filename=file, local_dir=local_dir)
+			gguf_path = await asyncio.to_thread(hf_hub_download, repo_id=repo_id, filename=file, local_dir=local_dir)
 		if download_only:
-			continue
+			return
 
 		reader = gguf.GGUFReader(gguf_path)
 		print(f'Converting {gguf_path} to safetensors...')
-		gguf_to_safetensors(reader.tensors, gguf_path.replace('.gguf', '.safetensors'))
+		await asyncio.to_thread(gguf_to_safetensors, reader.tensors, gguf_path.replace('.gguf', '.safetensors'))
+
+	async def main_async():
+		tasks = [asyncio.create_task(download_and_convert(file)) for file in gguf_files]
+		await asyncio.gather(*tasks)
+
+	asyncio.run(main_async())
 
 	print('Done.')
 
